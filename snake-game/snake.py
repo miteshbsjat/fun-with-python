c35b04e2baf9ce8fd1572c1f4549f0c8d8134779
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2020-12-14 13:22:27 +0530

    -made initial positions of snake and food smaller values of x and y so it fits in a smaller window _8_8_

--- snake-game/snake.py
+++ snake-game/snake.py
@@ -16,8 +16,8 @@ win.border(0)
 win.nodelay(1) # -1
 
 # snake and food
-snake = [(4, 10), (4, 9), (4, 8)]
-food = (10, 15)
+snake = [(4, 4), (4, 3), (4, 2)]
+food = (6, 6)
 
 win.addch(food[0], food[1], '#')
 # game logic
@@ -65,7 +65,7 @@ while key != ESC:
         score += 1
         food = ()
         while food == ():
-            food = (randint(1,width), randint(1,height))
+            food = (randint(1,height), randint(1,width))
             if food in snake:
                 food = ()
         win.addch(food[0], food[1], '#')
